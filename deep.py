ac40f3f7cc77b1e8c3d656787ad759e9bb4a5c0b
Beata Turczuk <b.turczukk@gmail.com>
2017-03-09 11:36:09 +0100

    w2v appears to work
    Signed-off-by: Beata Turczuk <b.turczukk@gmail.com>

--- deep.py
+++ deep.py
@@ -4,9 +4,12 @@ import logging
 import csv
 import re
 import pandas as pd
+import numpy as np
 from unidecode import unidecode
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from gensim.models import KeyedVectors
 
 class Crawler:
     def __init__(self, input_file, output_file_train, output_file_test):
@@ -107,11 +110,13 @@ class Crawler:
         outfile_test.to_csv(self.output_file_test, quoting=3, index_label='id', sep='\t')
         outfile_train.to_csv(self.output_file_train, quoting=3, index_label='id', sep='\t')
 
-class BOW:
-    def __init__(self, stop_words_file, file_name_train, file_name_test, output_file):
+class GetWordVectors:
+    def __init__(self, stop_words_file, file_name_train, file_name_test, output_file, word2vec_model_file, num_features=300, mode='w2v'):
         self.logger = logging.getLogger('main.' + self.__class__.__name__)
 
         self.output_file = output_file
+        self.num_features = num_features
+        self.mode = mode
 
         self.logger.info('Reading input train data from file: {}'.format(file_name_train))
         self.train = pd.read_csv(file_name_train, header=0, delimiter='\t', quoting=3)
@@ -146,15 +151,31 @@ class BOW:
             self.clean_test.append(self.clean_review(self.test['review'][i]))
         self.logger.debug(' 100% done!')
         
-        # create model
-        self.create_bag_of_word()
+        self.result_bow = None
+        self.result_w2v = None
 
-        # run classification
-        self.classify()
+        if self.mode == 'bow' or self.mode == 'both':
+            # run classification
+            train_feature_vecs = self.get_feature_vec()
+            self.classify(train_feature_vecs)
 
-        # run tests
-        self.run_test()
-        
+            # run tests
+            self.result_bow = self.run_test_bow()
+
+        elif self.mode == 'w2v' or self.mode == 'both':
+            # load W2V model
+            self.load_word_2_vec(word2vec_model_file)
+            
+            train_feature_vecs = self.get_avg_feature_vec(self.clean_train)
+            test_feature_vecs = self.get_avg_feature_vec(self.clean_test)
+
+            self.classify(train_feature_vecs)
+
+            self.result_w2v = self.run_test_w2v(test_feature_vecs)
+        else:
+            self.logger.critical('Mode not recognized: {}'.format(self.mode))
+            exit(1)
+            
         # write_results_to_file
         self.write_results()
 
@@ -178,62 +199,131 @@ class BOW:
         # back to strings
         return (' '.join(meaningful_words))
 
-    def create_bag_of_word(self):
+    def load_word_2_vec(self, word2vec_model_file):
+        self.logger.info('Word 2 vec model loading: {}'.format(word2vec_model_file))
+        self.model = KeyedVectors.load_word2vec_format(word2vec_model_file, binary=True)
+
+    def make_feature_vec(self, words):
+        self.logger.debug('Make feature vectors')
+        feature_vec = np.zeros((self.num_features,), dtype='float32')
+
+        nwords = 0
+
+        # index to word set
+        i2w_set = set(self.model.index2word)
+
+        for word in words:
+            if word in i2w_set:
+                nwords += 1
+                feature_vec = np.add(feature_vec, self.model[word])
+
+        feature_vec = np.divide(feature_vec, nwords)
+        return feature_vec
+
+    def get_avg_feature_vec(self, clean_reviews):
+        self.logger.info('Get avarage feature vector')
+        counter = 0
+
+        review_feature_vec = np.zeros((len(clean_reviews), self.num_features), dtype='float32')
+
+        review_len = len(clean_reviews)
+        review_len_10percent = int(review_len/10)
+        for review in clean_reviews:
+            if not counter % review_len_10percent:
+                self.logger.info('{:3.0f}% done!'.format(counter/review_len * 100))
+            review_feature_vec[counter] = self.make_feature_vec(review)
+            counter += 1
+        self.logger.info('100% done!')
+
+        return review_feature_vec
+
+    def get_feature_vec(self):
         self.logger.info('Create count vectorizer')
         self.vectorizer = CountVectorizer(analyzer='word',
                                           tokenizer=None,
                                           preprocessor=None,
                                           stop_words=None,
-                                          max_features=300)
+                                          max_features=self.num_features)
 
         self.logger.info('Fit transform')
-        self.train_data_features = self.vectorizer.fit_transform(self.clean_train)
+        train_data_features = self.vectorizer.fit_transform(self.clean_train)
 
         self.logger.info('To array..')
-        self.train_data_features = self.train_data_features.toarray()
+        train_data_features = train_data_features.toarray()
 
-    def classify(self):
+        return train_data_features
+
+    def classify(self, train_data_vec):
         self.logger.info('Create classifier')
-        self.forest = RandomForestClassifier(n_estimators=100)
+#        self.forest = RandomForestClassifier(n_estimators=100)
+        self.regression = LogisticRegression(n_jobs=8)
 
         self.logger.info('Fit classifier')
-        self.forest = self.forest.fit(self.train_data_features, self.train['score'])
+#        self.forest = self.forest.fit(train_data_vec, self.train['score'])
+        self.regression = self.regression.fit(train_data_vec, self.train['score'])
         
-    def run_test(self):
+    def run_test_bow(self):
         self.logger.info('Run tests...')
-        self.test_data_features = self.vectorizer.transform(self.clean_test)
+        test_data_features = self.vectorizer.transform(self.clean_test)
 
         self.logger.info('To array...')
-        self.test_data_features = self.test_data_features.toarray()
+        test_data_features = test_data_features.toarray()
 
         self.logger.info('Predict...')
-        self.result = self.forest.predict(self.test_data_features)
+        result = self.regression.predict(test_data_features)
+
+        return result
+
+    def run_test_w2v(self, test_feature_vecs):
+        self.logger.info('Predict...')
+        result = self.regression.predict(test_feature_vecs)
+
+        return result
 
     def write_results(self):
         self.logger.info('Write results to file: {}'.format(self.output_file))
-        output = pd.DataFrame(data={'review': self.clean_test, 'score_predict':self.result, 'score':self.test['score']})
+        if self.mode == 'bow':
+            data = {'review': self.clean_test, 'score_predict_bow':self.result_bow, 'score':self.test['score']}
+        elif self.mode == 'w2v':
+            data = {'review': self.clean_test, 'score_predict_w2v':self.result_w2v, 'score':self.test['score']}
+        elif self.mode == 'both':
+            data = {'review': self.clean_test, 'score_predict_bow':self.result_bow, 'score_predict_w2v': self.result_w2v, 'score':self.test['score']}
 
-        score = 0
-        for i in range(0, len(self.result)):
-            if int(self.result[i]) >= 6 and int(self.test['score'][i] >= 6):
-                score += 1
-            elif int(self.result[i]) < 5 and int(self.test['score'][i] < 5):
-                score += 1
-        
+        output = pd.DataFrame(data)
+
         self.logger.info('Size of training: {}'.format(len(self.clean_train)))
         self.logger.info('Size of test: {}'.format(len(self.clean_test)))
-        self.logger.info('Predicted correct: {}/{} {:3.2f}%'.format(score, len(self.clean_test), score/len(self.clean_test)*100))
+
+        if self.mode == 'bow' or self.mode == 'both':
+            score = 0
+            for i in range(0, len(self.result_bow)):
+                if int(self.result_bow[i]) >= 6 and int(self.test['score'][i] >= 6):
+                    score += 1
+                elif int(self.result_bow[i]) < 5 and int(self.test['score'][i] < 5):
+                    score += 1
+        
+            self.logger.info('Predicted bow correct: {}/{} {:3.2f}%'.format(score, len(self.clean_test), score/len(self.clean_test)*100))
+        if self.mode == 'w2v' or self.mode == 'both':
+            score = 0
+            for i in range(0, len(self.result_w2v)):
+                if int(self.result_w2v[i]) >= 6 and int(self.test['score'][i] >= 6):
+                    score += 1
+                elif int(self.result_w2v[i]) < 5 and int(self.test['score'][i] < 5):
+                    score += 1
+
+            self.logger.info('Predicted w2v correct: {}/{} {:3.2f}%'.format(score, len(self.clean_test), score/len(self.clean_test)*100))
 
         output.to_csv(self.output_file, index=False, quoting=3)
-
 
 if __name__ == '__main__':
     logging.basicConfig()
     logger = logging.getLogger('main')
     logger.setLevel(logging.DEBUG)
     c = Crawler('input.txt', 'output_train.csv', 'output_test.csv')
-    b = BOW(stop_words_file='stopwords2.txt',
+    b = GetWordVectors(stop_words_file='stopwords2.txt',
             file_name_train='output_train.csv',
             file_name_test='output_test.csv',
-            output_file='output.csv')
+            output_file='output.csv',
+            word2vec_model_file='input.bin',
+            mode='w2v')
 
